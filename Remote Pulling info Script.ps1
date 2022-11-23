691072fb85e2968e5683e4141aafce52717ec8df
Kleinbp <57073756+Kleinbp@users.noreply.github.com>
2022-11-23 19:33:18 +0100

    Move values to correct location
    $Values were on the wrong line and would produce errors in output as values were not passed to output.

--- Remote Pulling info Script.ps1
+++ Remote Pulling info Script.ps1
@@ -26,35 +26,25 @@ $Outputfile = 'C:\Users\public\Documents\Output.csv'
 
 #Formatting the info for the CSV
 $Output = New-Object -TypeName PSobject
-$output | Add-Member -MemberType NoteProperty -Name 'Computer Name' -Value
-$ComputerName.PScomputername
+$output | Add-Member -MemberType NoteProperty -Name 'Computer Name' -Value $ComputerName.PScomputername
 
-$output | Add-Member -MemberType NoteProperty -Name 'Serial Number' -Value
-$ComputerInfo.BiosSeralNumber
+$output | Add-Member -MemberType NoteProperty -Name 'Serial Number' -Value $ComputerInfo.BiosSeralNumber
 
-$output | Add-Member -MemberType NoteProperty -Name 'IP Address' -Value
-$IP.IPv4Address.IPaddress
+$output | Add-Member -MemberType NoteProperty -Name 'IP Address' -Value $IP.IPv4Address.IPaddress
 
-$output | Add-Member -MemberType NoteProperty -Name 'Build Version' -Value
-$Computerinfo.WindowsBuildLabex
+$output | Add-Member -MemberType NoteProperty -Name 'Build Version' -Value $Computerinfo.WindowsBuildLabex
 
-$output | Add-Member -MemberType NoteProperty -Name 'Windows Version' -Value
-$Computerinfo.windowsversion
+$output | Add-Member -MemberType NoteProperty -Name 'Windows Version' -Value $Computerinfo.windowsversion
 
-$output | Add-Member -MemberType NoteProperty -Name 'Network Interface' -Value
-$NetworkAdapterinfo.InferfaceDescription
+$output | Add-Member -MemberType NoteProperty -Name 'Network Interface' -Value $NetworkAdapterinfo.InferfaceDescription
 
-$output | Add-Member -MemberType NoteProperty -Name 'MAC' -Value
-$NetworkAdapterinfo.MacAddress
+$output | Add-Member -MemberType NoteProperty -Name 'MAC' -Value $NetworkAdapterinfo.MacAddress
 
-$output | Add-Member -MemberType NoteProperty -Name 'Inferface Speed' -Value
-$NetworkAdapterinfo.Linkspeed
+$output | Add-Member -MemberType NoteProperty -Name 'Inferface Speed' -Value $NetworkAdapterinfo.Linkspeed
 
-$output | Add-Member -MemberType NoteProperty -Name 'Users w/ profile on system' -Value
-$Listofusers.name
+$output | Add-Member -MemberType NoteProperty -Name 'Users w/ profile on system' -Value $Listofusers.name
 
-$output | Add-Member -MemberType NoteProperty -Name 'DNS Servers' -Value
-$IP.DNSSERVER.Serveraddress
+$output | Add-Member -MemberType NoteProperty -Name 'DNS Servers' -Value $IP.DNSSERVER.Serveraddress
 
 #Outputting the pulled info into a CSV
 $Output | ConvertTo-CSV -NoTypeInformation | out-file $Outputfile -Append
